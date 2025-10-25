7b47b09cd3be3596a5ec51a7636e3913beca772d
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-26 00:27:15 +0300

    test: test in the console

---
+++ src/main/java/com/example/cqrs_ecommerce/Main.java
@@ -0,0 +1,23 @@
+package com.example.cqrs_ecommerce;
+
+import com.example.cqrs_ecommerce.domain.Money;
+import com.example.cqrs_ecommerce.domain.Product;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args){
+        //  bu şekilde oluşturulamıyor artık, private access
+        // Product product = new Product();
+
+        Product product = Product.create(
+                "ürün 1",
+                new Money(BigDecimal.valueOf(350),"TRY"),
+                "Ürün 1 açıklaması",
+                140
+        );
+        product.setName("ürün 2");
+        System.out.println(product.name());
+    }
+}
+
