90d261e8a6b3d661ce6cbe851c085cde4d773d07
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-25 23:43:21 +0300

    feat: add record Money

---
+++ src/main/java/com/example/cqrs_ecommerce/domain/Money.java
@@ -0,0 +1,18 @@
+package com.example.cqrs_ecommerce.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+// Class'ın değiştirilemez (immutable) hali
+//Value Object: Değer Nesnesi
+public record Money(BigDecimal amount, String currency) {
+    public Money {
+        Objects.requireNonNull(amount);
+        Objects.requireNonNull(currency);
+        // Objects'te istediğin kural yoksa saf java ile de yazabilirsin
+        if (amount.signum() < 0)
+            throw new IllegalArgumentException("Amount must be positive");
+        if (currency.isBlank())
+            throw new IllegalArgumentException("Currency cannot be blank.");
+    }
+}
