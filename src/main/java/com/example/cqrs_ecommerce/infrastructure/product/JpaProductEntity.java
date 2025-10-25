df4a212095356b7d3281effe93517ca294af45df
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-26 01:25:15 +0300

    feat: infrastructure layer dependency added, Jpa Entity added

---
+++ src/main/java/com/example/cqrs_ecommerce/infrastructure/product/JpaProductEntity.java
@@ -0,0 +1,10 @@
+package com.example.cqrs_ecommerce.infrastructure.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class JpaProductEntity {
+
+}
