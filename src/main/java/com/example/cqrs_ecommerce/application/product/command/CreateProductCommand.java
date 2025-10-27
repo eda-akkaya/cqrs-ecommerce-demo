5697876e176594413a7691f7947653711c89aba9
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-28 01:18:30 +0300

    feat: add validation to CreateProductCommand

--- src/main/java/com/example/cqrs_ecommerce/application/product/command/CreateProductCommand.java
+++ src/main/java/com/example/cqrs_ecommerce/application/product/command/CreateProductCommand.java
@@ -3,12 +3,19 @@ package com.example.cqrs_ecommerce.application.product.command;
 import com.example.cqrs_ecommerce.application.product.dto.CreatedProductResponse;
 import com.example.cqrs_ecommerce.core.cqrs.Command;
 import com.example.cqrs_ecommerce.domain.product.repository.ProductRepository;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
 public record CreateProductCommand(
-        String name, String description, BigDecimal price,
-        String currency, Integer stock
+        @NotBlank @Size(min=3, max = 255) String name,
+        @NotBlank @Size(min=3, max = 255) String description,
+        @Positive BigDecimal price,
+        @NotBlank @Size(min = 2, max = 255) String currency,
+        @Min(0) Integer stock
 ) implements Command<CreatedProductResponse> { //marker interface
 
 
