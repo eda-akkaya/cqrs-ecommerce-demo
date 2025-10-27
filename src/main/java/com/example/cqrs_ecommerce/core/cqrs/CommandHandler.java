442a30be245f8e1c558fb785d07ba4a53ecdbcb0
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-28 01:20:40 +0300

    fix: type of method parameter

--- src/main/java/com/example/cqrs_ecommerce/core/cqrs/CommandHandler.java
+++ src/main/java/com/example/cqrs_ecommerce/core/cqrs/CommandHandler.java
@@ -1,8 +1,8 @@
 package com.example.cqrs_ecommerce.core.cqrs;
 
 
-// Q tipi Komutu alıp işler. Genellikle geriye değer döndürmez (void)
+// C tipi Komutu alıp işler. Genellikle geriye değer döndürmez (void)
 // veya işlem sonucu için bir Result nesnesi döndürür.
-public interface CommandHandler<Q extends Command<R>, R>{
-    R handle(Q query);
+public interface CommandHandler<C extends Command<R>, R>{
+    R handle(C query);
 }
